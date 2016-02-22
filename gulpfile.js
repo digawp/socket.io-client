socketio/socket.io

added error code capturing for zuul child process

gulpfile.js
@@ -79,7 +79,9 @@ function testZuul() {
     args.push(process.env.BROWSER_PLATFORM);
   }
   args.push(TEST_FILE);
-  return child.spawn(ZUUL_CMD, args, { stdio: "inherit" });
+  const zuulChield = child.spawn(ZUUL_CMD, args, { stdio: "inherit" });
+  zuulChild.on("exit", function (code) { process.exit(code); });
+  return zuulChild;
 }
 
 function testNode() {

digawp/socket.io-client

gulpfile.js
@@ -79,7 +79,9 @@ function testZuul() {
     args.push(process.env.BROWSER_PLATFORM);
   }
   args.push(TEST_FILE);
-  return child.spawn(ZUUL_CMD, args, { stdio: "inherit" });
+  const zuulChild = child.spawn(ZUUL_CMD, args, { stdio: "inherit" });
+  zuulChild.on("exit", function (code) { process.exit(code); });
+  return zuulChild;
 }
 
 function testNode() {